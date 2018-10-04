ea9b28d934cf1fdeeab06f0469abcf40e489f46d
Marcormio <m.cormio@campus.unimib.it>
2018-10-04 16:22:16 +0200

    cytoscape initialization

--- js/renderer.js
+++ js/renderer.js
@@ -1,3 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var cytoscape = require('cytoscape');
+
+var cy = cytoscape({
+  container: document.getElementById('cy') // container to render in
+});
